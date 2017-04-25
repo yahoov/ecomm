4e76ef3f137cd2fa5eae283f6198d9c6a65ab098
Vikram Anand <vikram_bvicam@yahoo.com>
2017-04-25 17:47:25 +0530

    successful payment will show order summary

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,25 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
+  def show
+    @order = Order.find(params[:order_id])
+  end
+
   def new
     @items = current_user.cart.items
   end
 
   def create
     items = current_user.cart.items
-    result = OrderService.new(current_user, items).create_order
+    order = OrderService.new(current_user, items).create_order
 
-    if result
+    if order
       session[:cart] = {}
       session_hash = { cart: { 'products' => [] } }
       CartService.new(current_user, session_hash[:cart]).create_or_update_cart
-      flash[:success] = 'Thank you! Your order has been successfully placed!'
-      flash.keep(:success)
-      redirect_to root_path
+      # flash[:success] = 'Thank you! Your order has been successfully placed!'
+      # flash.keep(:success)
+      redirect_to order_summary_path(order_id: order.id)
     else
       flash[:alert] = 'Something went wrong. Please try again in sometime.'
       flash.keep(:alert)
--- spec/features/orders/order_management_spec.rb
+++ spec/features/orders/order_management_spec.rb
@@ -28,12 +28,52 @@ feature "User can place order" do
 
     user_sign_in user
     expect(page).to have_selector("div", text: data[:name])
-    all('a', :text => 'Add to cart').last.click
+    all('a', :text => 'Add to cart').first.click
     expect(page).to have_content("My Cart (1)")
     click_link 'My Cart (1)'
     click_link 'Proceed to checkout'
     expect(page).to have_content("Confirm Order")
     click_link 'Make Payment'
     expect(page).to have_content("Your order has been successfully placed!")
+  end
+end
+
+feature "Order summary is displayed to the user after successfully placing order" do
+  include Features::Admin::SessionHelpers
+  include Features::User::SessionHelpers
+  include Features::Admin::ProductHelpers
+
+  given!(:admin) { FactoryGirl.create(:admin) }
+  given!(:user) { FactoryGirl.create(:user) }
+
+  scenario "with products in cart" do
+    admin_sign_in admin
+    expect(page).to have_content "Signed in successfully"
+    data = {
+      name: Faker::Name.unique.name,
+      description: Faker::Lorem.characters(100),
+      price: Faker::Number.number(3),
+      quantity: Faker::Number.number(2),
+      maker: Faker::Name.unique.name
+    }
+
+    create_product_with data
+    expect(page).to have_content("Product has been successfully created")
+    click_link 'Sign Out'
+
+    user_sign_in user
+    expect(page).to have_selector("div", text: data[:name])
+    all('a', :text => 'Add to cart').first.click
+    expect(page).to have_content("My Cart (1)")
+    click_link 'My Cart (1)'
+    click_link 'Proceed to checkout'
+    expect(page).to have_content("Confirm Order")
+    click_link 'Make Payment'
+    within('div.order_summary') do
+      expect(page).to have_selector("a", text: data[:name])
+      expect(page).to have_selector("td", text: '1')
+      expect(page).to have_selector("td", text: data[:price])
+      expect(page).to have_selector("td", text: "#{1 * data[:price].to_f}")
+    end
   end
 end
