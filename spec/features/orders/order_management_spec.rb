cc52a103dfba3d21db0922fbb9e8959d9345f061
Vikram Anand <vikram_bvicam@yahoo.com>
2017-04-25 21:22:04 +0530

    spec added for testing stock decrement

--- spec/features/orders/order_management_spec.rb
+++ spec/features/orders/order_management_spec.rb
@@ -77,3 +77,40 @@ feature "Order summary is displayed to the user after successfully placing order
     end
   end
 end
+
+feature "Product count decreases after making payment for an order" do
+  include Features::Admin::SessionHelpers
+  include Features::User::SessionHelpers
+  include Features::Admin::ProductHelpers
+
+  given!(:admin) { FactoryGirl.create(:admin) }
+  given!(:user) { FactoryGirl.create(:user) }
+
+  scenario "with products in cart" do
+    admin_sign_in admin
+    expect(page).to have_content "Signed in successfully"
+    data = {
+      name: Faker::Name.unique.name,
+      description: Faker::Lorem.characters(100),
+      price: Faker::Number.number(2),
+      quantity: 112,
+      maker: Faker::Name.unique.name
+    }
+
+    create_product_with data
+    expect(page).to have_content("Product has been successfully created")
+    click_link 'Sign Out'
+
+    user_sign_in user
+    expect(page).to have_selector("div", text: data[:name])
+    all('a', :text => 'Add to cart').first.click
+    expect(page).to have_content("My Cart (1)")
+    click_link 'My Cart (1)'
+    click_link 'Proceed to checkout'
+    expect(page).to have_content("Confirm Order")
+    click_link 'alias_to_make_payment'
+    visit '/'
+    click_link data[:name]
+    expect(page).to have_content("111") ## 1 less than original quantity/stock
+  end
+end
