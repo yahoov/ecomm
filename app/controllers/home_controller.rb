a85ed37dd278921962c30905d45d0b3b84e66fdc
Vikram Anand <vikram_bvicam@yahoo.com>
2017-04-26 06:27:00 +0530

    data available only when products exists

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @products = Product.order("created_at DESC").limit(6)
+    @last_dt = @products.any? ? @products.last.created_at : ''
     @load_more = Product.all.count > 6
   end
 
